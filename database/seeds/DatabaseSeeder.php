f3e2e6ab196dae0412600de2f88a47ecff13c814
Jeff269 <mini4tur4@gmail.com>
2018-04-28 20:59:35 -0500

    nada

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,5 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MaterialAreasTableSeeder::class);
         $this->call(BookcaseMaterialsTableSeeder::class);
         $this->call(MaterialAuthorsTableSeeder::class);
+
     }
 }
