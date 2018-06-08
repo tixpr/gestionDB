b143fccfa244ed14e06307c4d92f50fd9b5eb732
Lizandro Meza Espinoza <lizandromesp@gmail.com>
2018-06-08 11:42:46 -0500

    cambios

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -26,8 +27,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Route::middleware(['cors','api'])->group(function (){
+            Passport::routes();
+        });
 
         //
-        Passport::routes();
-    }
+            }
 }
