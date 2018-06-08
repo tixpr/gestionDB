9f39e358a1895fee4413a0b4ca0e74325a7c669d
Adderlyn Tito Palacios Rojas <www.adderlyn@gmail.com>
2018-06-08 11:32:20 -0500

    configuración api

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -25,8 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+		Route::middleware(['cors','api'])->group(function(){
+			Passport::routes();	
+		});
 		//
-		Passport::routes();
     }
 }
