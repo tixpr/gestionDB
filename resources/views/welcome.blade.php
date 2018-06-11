954ddf53280afe93d8e9334893e4aafc651bbe47
Jan Francis Arroyo Cuaquira <franxis1952@gmail.com>
2018-06-11 16:43:14 -0500

    cambios 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,32 +9,96 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		<link href="/css/estilo.css" rel="stylesheet" type="text/css">
 		<script src="/js/majax.js"></script>
     </head>
     <body>
-		<button id="btn">
-			Obtener datos
-		</button>
-		<div id="contenido">
-		</div>
-		<script>
-			Majax.setConfig(2, 'u8eZrTBppnU4JXdPUymRDHynS5KApTvMV4rTYni3','');
-			function obtenerMateriales(e){
-				e.preventDefault();
-				var majax = new Majax();
-				majax.get(
-					'/api/materials',
-					{
-						valid: function(r){
-							console.info(r);
-						},
-						error: function(error){
-							console.error(error);
-						}
-					}
-				);
-			}
-			document.getElementById('btn').addEventListener('click',obtenerMateriales);
-		</script>
+	<div id="contenedor">
+        <button id="btn01" class="boton">
+            Obtener datos
+        </button>
+        <button id="btn02" class="boton">
+            Obtener datos lenguajes
+        </button>
+        <button id="btn03" class="boton">
+            Obtener datos Tipomaterial
+        </button>
+    </div>
+        <script>
+            Majax.setConfig(2, 'Rs3KbBqSyEsV39Pd9LdyAX6EVHg5kUnuf6SuMZsQ','');
+            function obtenerMateriales(e){
+                e.preventDefault();
+                var majax = new Majax();
+                majax.get(
+                    '/api/materials',
+                    {
+                        valid: function(r){
+                            console.info(r);
+                            ds1 = document.getElementById('s1');
+                            r.data.forEach(function(s){
+                                ds1.innerHTML = ds1.innerHTML + "TITULO: "+ s.titulo + "  IDIOMA: "+ s.idioma + "  TIPO: "+ s.tipo;
+                            });
+                            console.info(r.data);
+                        },
+                        error: function(error){
+                            console.error(error);
+                        }
+                    }
+                );
+            }
+            document.getElementById('btn01').addEventListener('click',obtenerMateriales);
+        </script>
+
+        <script>
+            Majax.setConfig(2, 'Rs3KbBqSyEsV39Pd9LdyAX6EVHg5kUnuf6SuMZsQ','');
+            function obtenerLanguages(e){
+                e.preventDefault();
+                var majax = new Majax();
+                majax.get(
+                    '/api/languages',
+                    {
+                        valid: function(r){
+                            console.info(r);
+                            ds1 = document.getElementById('s1');
+                            r.data.forEach(function(s){
+                                ds1.innerHTML = ds1.innerHTML +"  IDIOMA: "+ s.lenguaje;
+                            });
+                            console.info(r.data);
+                        },
+                        error: function(error){
+                            console.error(error);
+                        }
+                    }
+                );
+            }
+            document.getElementById('btn02').addEventListener('click',obtenerLanguages);
+        </script>
+
+        <script>
+            Majax.setConfig(2, 'Rs3KbBqSyEsV39Pd9LdyAX6EVHg5kUnuf6SuMZsQ','');
+            function obtenerMaterialType(e){
+                e.preventDefault();
+                var majax = new Majax();
+                majax.get(
+                    '/api/materialstype',
+                    {
+                        valid: function(r){
+                            console.info(r);
+                            ds1 = document.getElementById('s1');
+                            r.data.forEach(function(s){
+                                ds1.innerHTML = ds1.innerHTML +"TIPO: "+ s.tipo + "</br>";
+                            });
+                            console.info(r.data);
+                        },
+                        error: function(error){
+                            console.error(error);
+                        }
+                    }
+                );
+            }
+            document.getElementById('btn03').addEventListener('click',obtenerMaterialType);
+        </script>
+        <div id="s1"></div>
+
     </body>
 </html>
