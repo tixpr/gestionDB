74a4aea160c33866c4c2e6a251b99787a8ebd147
Frank Serrano Basilio <frankserranobasilio@gmail.com>
2018-06-10 22:30:56 -0500

    0

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'library1'),
+            'database' => env('DB_DATABASE', 'library'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', '123456'),
             'unix_socket' => env('DB_SOCKET', ''),
