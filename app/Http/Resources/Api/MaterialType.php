d37277b97ee7e03ef63c44c60e1de1083ccd01f7
ruthbetzabe <luzbetza5@gmail.com>
2018-06-11 16:31:27 -0500

    tarea 08-06-18

---
+++ app/Http/Resources/Api/MaterialType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MaterialType extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return[
+            'tipo' => $this->type
+        ];
+    }
+}
