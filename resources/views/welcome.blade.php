51f00b4c33b1a8b0078b5e3f3debec5d337a9cf1
Walter Eusebio Arzapalo Rey <www.wearcito23@gmail.com>
2018-06-12 16:44:46 -0500

    Tarea 06-08-2018

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
 	<header>
-		<h1><marquee behavior="alternate">SERVICIO DE CONSULTA	</marquee>
+		<h1><marquee behavior="alternate">SERVICIO DE CONSULTA</marquee>
 	</header>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,124 +14,102 @@
 
     </head>
     <body>
-        <ul id="buttons">
-            <a id="btn1"><p>MATERIAL</p></a>
-            <a id="btn2"><p>IDIOMAS</p></a>
-            <a id="btn3"><p>TIPO DE MATERIALES</p></a>
-            <a id="btn4"><p>USUARIOS</p></a>
+       <div id="contenido">
+       <ul id="botones">
+            <b id="btn1"><li>MATERIALES</li></b>
+            <b id="btn2"><li>IDIOMAS</li></b>
+            <b id="btn3"><li>TIPO DE MATERIALES</li></b>
+            <b id="btn4"><li>USUARIOS</li></b>
+            <b id="btn5" value="Actualizar pagina" onclick="window.location.reload()"><li>REFRESCAR</li></b>
         </ul>
+        </div>
         <script>
-            Majax.setConfig(2,'v6SYRt3gvXVj5wPW7gGLOmdlBF2fi6I0fmNFT9J8','');
-            function obetenerMateriales(e) {
-                e.preventDefault();
-                var majax= new Majax();
-                majax.get(
-                    '/api/materials',
-                    {
-                        valid:function (r) {
-                            // Esta Parte del Codigo es para que se oculten los otros cuando uses el Boton
-                            document.getElementById('s1').style.display = 'flex';
-                            document.getElementById('s2').style.display = 'none';
-                            document.getElementById('s3').style.display = 'none';
-                            document.getElementById('s4').style.display = 'none';
-                            //document.getElementsByClassName('tarjeta').style.border='1px solid green';
-                            //Esta parte es para imprimir en el HTML
-                            ds1 =   document.getElementById('s1');
-                            r.data.forEach(function (s) {
-                                ds1.innerHTML = ds1.innerHTML +"<div class='tarjeta'>"+"<h1>"+s.titulo+"</h1>"+"<h2>"+s.idioma+"</h2>"+"<h3>"+s.tipo+"</h3>"+"<h4>"+s.resumen+"</h4>"+"</div>";
-                            });
-                        },
-                        error: function(error){
-                            console.error(error);
-                        }
-                    }
-                );
-            }
-            document.getElementById('btn1').addEventListener('click',obetenerMateriales);
+			Majax.setConfig(2, '6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+			function obtenerMateriales(e){
+				e.preventDefault();
+				var majax = new Majax();
+				majax.get(
+					'/api/materials',
+					{
+						valid: function(r){
+							console.info(r);
+                            document.getElementById("m").innerHTML=JSON.stringify(r);
+						},
+						error: function(error){
+							console.error(error);
+						}
+					}
+				);
+			}
+			document.getElementById('btn1').addEventListener('click',obtenerMateriales);
         </script>
+        <div id="m">
+        </div>  
         <script>
-            Majax.setConfig(2,'v6SYRt3gvXVj5wPW7gGLOmdlBF2fi6I0fmNFT9J8','');
-            function obetenerIdioma(e) {
-                e.preventDefault();
-                var majax= new Majax();
-                majax.get(
-                    '/api/languages',
-                    {
-                        valid:function (r) {
-                            document.getElementById('s1').style.display = 'none';
-                            document.getElementById('s2').style.display = 'flex';
-                            document.getElementById('s3').style.display = 'none';
-                            document.getElementById('s4').style.display = 'none';
-                            //document.getElementsByClassName('tarjeta').style.border='1px solid blue';
-                            ds2 =   document.getElementById('s2');
-                            r.data.forEach(function (s) {
-                                ds2.innerHTML = ds2.innerHTML +"<div class='tarjeta'>"+"<h1>"+s.idioma+"</h1>"+"</div>";
-                            });
-                        },
-                        error: function(error){
-                            console.error(error);
-                        }
-                    }
-                );
-            }
-            document.getElementById('btn2').addEventListener('click',obetenerIdioma);
+			Majax.setConfig(2, '6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+			function obtenerIdiomas(e){
+				e.preventDefault();
+				var majax = new Majax();
+				majax.get(
+					'/api/languages',
+					{
+						valid: function(r){
+							console.info(r);
+                            document.getElementById("i").innerHTML=JSON.stringify(r);
+						},
+						error: function(error){
+							console.error(error);
+						}
+					}
+				);
+			}
+			document.getElementById('btn2').addEventListener('click',obtenerIdiomas);
         </script>
+        <div id="i">
+        </div>  
         <script>
-            Majax.setConfig(2,'v6SYRt3gvXVj5wPW7gGLOmdlBF2fi6I0fmNFT9J8','');
-            function obetenerTipoMaterial(e) {
-                e.preventDefault();
-                var majax= new Majax();
-                majax.get(
-                    '/api/materialTypes',
-                    {
-                        valid:function (r) {
-                            document.getElementById('s1').style.display = 'none';
-                            document.getElementById('s2').style.display = 'none';
-                            document.getElementById('s3').style.display = 'flex';
-                            document.getElementById('s4').style.display = 'none';
-                            //document.getElementsByClassName('tarjeta').style.border='1px solid red';
-                            ds3 =   document.getElementById('s3');
-                            r.data.forEach(function (s) {
-                                ds3.innerHTML = ds3.innerHTML +"<div class='tarjeta'>"+"<h1>"+s.tipo+"</h1>"+"</div>";
-                            });
-                        },
-                        error: function(error){
-                            console.error(error);
-                        }
-                    }
-                );
-            }
-            document.getElementById('btn3').addEventListener('click',obetenerTipoMaterial);
+			Majax.setConfig(2, '6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+			function obtenerTiposMateriales(e){
+				e.preventDefault();
+				var majax = new Majax();
+				majax.get(
+					'/api/materialTypes',
+					{
+						valid: function(r){
+							console.info(r);
+                            document.getElementById("t").innerHTML=JSON.stringify(r);
+						},
+						error: function(error){
+							console.error(error);
+						}
+					}
+				);
+			}
+			document.getElementById('btn3').addEventListener('click',obtenerTiposMateriales);
         </script>
+        <div id="t">
+        </div>  
         <script>
-            Majax.setConfig(2,'v6SYRt3gvXVj5wPW7gGLOmdlBF2fi6I0fmNFT9J8','');
-            function obetenerUsuario(e) {
-                e.preventDefault();
-                var majax= new Majax();
-                majax.get(
-                    '/api/users',
-                    {
-                        valid:function (r) {
-                            document.getElementById('s1').style.display = 'none';
-                            document.getElementById('s2').style.display = 'none';
-                            document.getElementById('s3').style.display = 'none';
-                            document.getElementById('s4').style.display = 'flex';
-                            ds4 =   document.getElementById('s4');
-                            r.data.forEach(function (s) {
-                                ds4.innerHTML = ds4.innerHTML +"<div class='tarjeta'>"+"<h1>"+s.nombre+"</h1>"+"<h2>"+s.correo+"</h2>"+"</div>";
-                            });
-                        },
-                        error: function(error){
-                            console.error(error);
-                        }
-                    }
-                );
-            }
-            document.getElementById('btn4').addEventListener('click',obetenerUsuario);
+			Majax.setConfig(2, '6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+			function obtenerUsuarios(e){
+				e.preventDefault();
+				var majax = new Majax();
+				majax.get(
+					'/api/users',
+					{
+						valid: function(r){
+							console.info(r);
+                            document.getElementById("u").innerHTML=JSON.stringify(r);
+						},
+						error: function(error){
+							console.error(error);
+						}
+					}
+				);
+			}
+			document.getElementById('btn4').addEventListener('click',obtenerUsuarios);
         </script>
-        <section id="s1"></section>
-        <section id="s2"></section>
-        <section id="s3"></section>
-        <section id="s4"></section>
+        <div id="u">
+        </div>
     </body>
 </html>
